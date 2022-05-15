6adbc6560dcc778091ab41274aaad38b984997f9
den_hope <den_hope@mail.ru>
2022-05-15 14:52:16 +0300

    feat: add artistQuestionView

---
+++ src/app/artistQuestionView.ts
@@ -0,0 +1,18 @@
+import Control from '../utils/Control';
+import { IArtistQuestionData } from './IArtistQuestionData ';
+
+export class ArtistQuestionView extends Control {
+  onAnswer: (index: number) => void;
+
+  constructor(parentNode: HTMLElement, questionData: IArtistQuestionData) {
+    super(parentNode);
+
+    const question = new Control(this.node, 'div', '', 'Кто написал картину?');
+    const answerButton = questionData.answers.map((el, i) => {
+      const button = new Control(this.node, 'button', '', i.toString());
+      button.node.onclick = () => {
+        this.onAnswer(i);
+      };
+    });
+  }
+}
--- src/app/gameField.ts
+++ src/app/gameField.ts
@@ -1,4 +1,6 @@
 import Control from '../utils/Control';
+import { ArtistQuestionView } from './artistQuestionView';
+import { IArtistQuestionData } from './IArtistQuestionData ';
 interface IQuizOptions {
   gameName: string;
   categoryIndex: number;
@@ -10,6 +12,7 @@ export class GameFieldPage extends Control {
   onBack: () => void;
   onHome: () => void;
   onFinish: (results: IQuizResults) => void;
+  progressIndicator: Control<HTMLElement>;
   constructor(parentNode: HTMLElement, gameOptions: IQuizOptions) {
     super(parentNode);
     console.log(gameOptions);
@@ -28,9 +31,28 @@ export class GameFieldPage extends Control {
     homeButton.node.onclick = () => {
       this.onHome();
     };
-    const finishButton = new Control(this.node, 'button', '', 'finish');
-    finishButton.node.onclick = () => {
+
+    this.progressIndicator = new Control(this.node, 'div', '', '');
+
+    const questions: Array<IArtistQuestionData> = [
+      { answers: [1, 2, 3, 4] },
+      { answers: [1, 2, 3, 4] },
+      { answers: [1, 2, 3, 4] },
+    ];
+    this.questionCycle(questions, 0, () => {
       this.onFinish({});
+    });
+  }
+  questionCycle(questions: Array<IArtistQuestionData>, index: number, onFinish: () => void) {
+    if (index >= questions.length) {
+      onFinish();
+      return;
+    }
+    this.progressIndicator.node.textContent = `${index + 1} /${questions.length}`;
+    const question = new ArtistQuestionView(this.node, questions[index]);
+    question.onAnswer = (answerIndex) => {
+      question.destroy();
+      this.questionCycle(questions, index + 1, onFinish);
     };
   }
 }
